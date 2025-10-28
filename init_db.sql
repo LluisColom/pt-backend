645990efd755a28a1dbfeb091880b59eac9badaf
Lluís Oriol Colom Nichols <lluis.colom@borderless-technologies.com>
2025-10-28 17:57:40 +0100

    Create basic tables

--- init_db.sql
+++ init_db.sql
@@ -3,3 +3,17 @@ CREATE DATABASE pollution_tracker;
 
 \connect pollution_tracker;
 
+CREATE TABLE sensors (
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    sensor_name VARCHAR(255),
+    sensor_location VARCHAR(255)
+);
+
+CREATE TABLE readings (
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    sensor_id INTEGER REFERENCES sensors(id),
+    timestamp TIMESTAMPTZ NOT NULL,
+    co2_level REAL NOT NULL
+);
+
+INSERT INTO sensors (sensor_name, sensor_location) VALUES ('Petroquimica Repsol', 'Tarragona');
