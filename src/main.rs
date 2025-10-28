a0d7dbff75c23c86562d167906636d469988c492
Lluís Oriol Colom Nichols <lluis.colom@borderless-technologies.com>
2025-10-28 18:27:57 +0100

    Simplify maiun

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,15 @@
+mod db;
+mod http;
+
 use axum::extract::State;
 use axum::routing::post;
 use axum::{Json, Router, routing::get};
-use chrono::{DateTime, Utc};
 use dotenv;
-use serde::{Deserialize, Serialize};
 use sqlx::PgPool;
 use sqlx::postgres::PgPoolOptions;
 
-#[derive(Debug, Serialize, Deserialize)]
-struct SensorReading {
-    id: i32,
-    sensor_id: i32,
-    timestamp: DateTime<Utc>, // ISO 8601 format
-    co2: f32,
-    temperature: f32,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-struct ReadingResponse {
-    status: u16,
-    error_msg: String,
-}
-
-impl ReadingResponse {
-    fn success() -> Self {
-        ReadingResponse {
-            status: 200,
-            error_msg: "".to_string(),
-        }
-    }
-
-    fn bad_request(msg: impl AsRef<str>) -> Self {
-        ReadingResponse {
-            status: 400,
-            error_msg: msg.as_ref().to_string(),
-        }
-    }
-
-    fn internal_error() -> Self {
-        ReadingResponse {
-            status: 500,
-            error_msg: "Internal server error".to_string(),
-        }
-    }
-}
+use db::{SensorReading, insert_reading};
+use http::ReadingResponse;
 
 #[tokio::main]
 async fn main() {
@@ -94,22 +60,11 @@ async fn sensor_reading(
     State(pool): State<PgPool>,
     Json(payload): Json<SensorReading>,
 ) -> Json<ReadingResponse> {
-    if payload.co2 < 0.0 {
-        return Json(ReadingResponse::bad_request("Invalid CO2 value"));
+    if let Err(reason) = db::validate_reading(&payload) {
+        return Json(ReadingResponse::bad_request(reason));
     }
 
-    if let Err(e) = sqlx::query!(
-        r#"
-        INSERT INTO readings (sensor_id, timestamp, co2_level)
-        VALUES ($1, $2, $3)
-        "#,
-        payload.sensor_id,
-        payload.timestamp,
-        payload.co2
-    )
-    .execute(&pool)
-    .await
-    {
+    if let Err(e) = insert_reading(&pool, payload).await {
         println!("Error inserting reading: {}", e);
         return Json(ReadingResponse::internal_error());
     }
