96e87eead07a331540f6dadd2cab7386ef66af21
Lluís Oriol Colom Nichols <lluis.colom@borderless-technologies.com>
2025-10-28 16:28:18 +0100

    Add DB support

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,26 @@
+use axum::extract::State;
 use axum::{Router, routing::get};
+use sqlx::PgPool;
+use sqlx::postgres::PgPoolOptions;
 
 #[tokio::main]
 async fn main() {
-    println!("Starting webserver");
+    // Load environment variables
+    dotenv::dotenv().ok();
+
+    // Connect to database
+    let db = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&db)
+        .await
+        .expect("Failed to connect to database");
 
     let app = Router::new()
         .route("/", get(root))
-        .route("/health", get(health_check));
+        .route("/health", get(health_check))
+        .route("/db_health", get(db_health_check))
+        .with_state(pool);
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
         .await
@@ -24,4 +38,11 @@ async fn root() -> &'static str {
 
 async fn health_check() -> &'static str {
     "OK"
+}
+
+async fn db_health_check(State(pool): State<PgPool>) -> &'static str {
+    match sqlx::query("SELECT 1").fetch_one(&pool).await {
+        Ok(_) => "OK",
+        Err(_) => "FAILED",
+    }
 }
