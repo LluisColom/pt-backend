5fac9abb692609c1bf76a9a2823c8c850e66c83b
Lluís Oriol Colom Nichols <lluis.colom@borderless-technologies.com>
2025-10-29 12:15:15 +0100

    Add temperature field

--- init_db.sql
+++ init_db.sql
@@ -13,7 +13,8 @@ CREATE TABLE readings (
     id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     sensor_id INTEGER REFERENCES sensors(id),
     timestamp TIMESTAMPTZ NOT NULL,
-    co2_level REAL NOT NULL
+    co2_level REAL NOT NULL,
+    temperature REAL NOT NULL
 );
 
 INSERT INTO sensors (sensor_name, sensor_location) VALUES ('Petroquimica Repsol', 'Tarragona');
