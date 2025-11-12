a5e142af14e14bffee5d15aff031798689362854
Lluís Oriol Colom Nichols <lluis.colom@borderless-technologies.com>
2025-11-12 18:08:08 +0100

    Update DB schema

--- init_db.sql
+++ init_db.sql
@@ -3,10 +3,20 @@ CREATE DATABASE pollution_tracker;
 
 \connect pollution_tracker;
 
+CREATE TABLE users (
+   id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+   username TEXT UNIQUE NOT NULL,
+   password TEXT NOT NULL,
+   role TEXT DEFAULT 'user',
+   created_at TIMESTAMP DEFAULT NOW()
+);
+
 CREATE TABLE sensors (
     id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     sensor_name VARCHAR(255),
-    sensor_location VARCHAR(255)
+    sensor_location VARCHAR(255),
+    user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+    created_at TIMESTAMP DEFAULT NOW()
 );
 
 CREATE TABLE readings (
@@ -17,14 +27,13 @@ CREATE TABLE readings (
     temperature REAL NOT NULL
 );
 
-CREATE TABLE users (
-   id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-   username TEXT UNIQUE NOT NULL,
-   password TEXT NOT NULL,
-   role TEXT DEFAULT 'user',
-   created_at TIMESTAMP DEFAULT NOW()
-);
-
+-- Indexes to enhance performance
 CREATE INDEX idx_sensor_readings_timestamp ON readings(sensor_id, timestamp);
+CREATE INDEX idx_sensor_user_id ON sensors(user_id);
 
-INSERT INTO sensors (sensor_name, sensor_location) VALUES ('Petroquimica Repsol', 'Tarragona');
+-- Insert test user
+INSERT INTO users (username, password, role)
+VALUES ('lluis', '$argon2id$v=19$m=19456,t=2,p=1$oz7KqblJDGMiSiaMPo9QyQ$QnuRj/P2YNuCUPdi2LrxBk/Kf85UwZj5qXxr6+zoJK8', 'user');
+
+-- Insert test sensor
+INSERT INTO sensors (sensor_name, sensor_location, user_id) VALUES ('Petroquimica Repsol', 'Tarragona', '1');
